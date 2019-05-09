b078f9de6769cb5e2bbe60231b2fee98c4d01a1a
NicoFlorschuetz <nflorschuetz0108@gmail.com>
2019-05-09 09:37:24 +0200

    switch to atom and platformiswitch to atom and platformio

--- MEGASIM/MegaSimulator/src/MegaSimulator.ino
+++ MEGASIM/MegaSimulator/src/MegaSimulator.ino
@@ -1,39 +1,39 @@
 #include "Arduino.h"
 #include <TimerOne.h>
 #include <Wire.h>
-#include "/Users/nicoflorschutz/Documents/Bachelorthesis/eigeneBib/classMega.h"
-
-
+//#include "/Users/nicoflorschutz/Documents/Bachelorthesis\ Kopie/eigeneBib/classMega.h"
+#include <classMega.h>
+//hahahahahahhah
 Simulator Mega1(9,8);
 
 void setup() {
-  
-  Wire.begin();
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.println("Setup begin");                                        
-  Mega1.setup_pins();
-  Mega1.search();
-  attachInterrupt(digitalPinToInterrupt(2), count_first, RISING);
-  attachInterrupt(digitalPinToInterrupt(3), count_second, RISING);
-  Timer1.initialize(4*1000000);
-  Timer1.attachInterrupt(keep_alive);
-  Serial.println("Setup end"); 
+
+        Wire.begin();
+        Serial.begin(9600);
+        while (!Serial);
+        Serial.println("Setup begin");
+        Mega1.setup_pins();
+        Mega1.search();
+        attachInterrupt(digitalPinToInterrupt(2), count_first, RISING);
+        attachInterrupt(digitalPinToInterrupt(3), count_second, RISING);
+        Timer1.initialize(4*1000000);
+        Timer1.attachInterrupt(keep_alive);
+        Serial.println("Setup end");
 }
 
- void loop() {
- 
-  Mega1.address_range();
- 
-} 
+void loop() {
+
+        Mega1.address_range();
+
+}
 
 void count_first(){
-  Mega1.setCounterOne(1);
-  
+        Mega1.setCounterOne(1);
+
 }
 
 void count_second(){
-  Mega1.setCounterTwo(1);
+        Mega1.setCounterTwo(1);
 }
 
 void keep_alive(){
@@ -49,7 +49,7 @@ void keep_alive(){
         }else{
                 Serial.println("Board 2 is missing");
         }
-        
+
         Mega1.setCounterOne(0);
         Mega1.setCounterTwo(0);
 }
