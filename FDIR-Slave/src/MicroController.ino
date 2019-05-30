b343c69de078b65b8dc695c0c63457e938268174
NicoFlorschuetz <nflorschuetz0108@gmail.com>
2019-05-30 15:07:49 +0200

    Slave send at first loop an message with connected pins and reset mode -- 0 for PinReset, 1 for WireReset -- at Failure Level 0 the slave do nothing because Level 0 is not implemented. At Level 1 the Slave is looking for the duration of the failure and at Level 2 the slave will reset the sensor and at Level 3 the slave send failure code 3 to master

--- FDIR-Slave/src/MicroController.ino
+++ FDIR-Slave/src/MicroController.ino
@@ -5,16 +5,16 @@
 volatile unsigned int counter= 0;
 int command = 0;
 
-#define TARGET1
+#define TARGET2
 #ifdef TARGET1
 //Id aufbau: ID[0] = Arduino id; ID[1]= fehler oder nicht ; ID[2] = fehler status
-int ID[] = {1, 1, 22, 1};
+int ID[] = {0,2, 0, 40};
 //The constructor of the class FDIR_Slave consists of GREEN_LED, RED_LED, ANALOG_PIN, ID, I2C_ADDRESS
 FDIR_Slave Arduino1(10, 8,  12, ID, 8);
 #endif
 
 #ifdef TARGET2
-int ID[] = {2, 0, 0, 0};
+int ID[] = {0, 3, 0, 30};
 FDIR_Slave Arduino1(10, 8,  12, ID, 9);
 #endif
 void setup() {
@@ -39,13 +39,8 @@ void setup() {
    }*/
 
 void keep_alive(){
-        //  int x = Wire.read();
-        //  if(x == '1') {
         digitalWrite(4,HIGH);
-
         digitalWrite(4,LOW);
-        //  }
-
 }
 
 void loop() {
@@ -58,39 +53,39 @@ void loop() {
 }
 
 
-int i = 0;
+
 void requestEvent()
 {
-        /*if (i == 0) {
-                Arduino1.ID[0] = 0;
-         #ifdef TARGET1
-                Arduino1.ID[1] = 0;
-                Arduino1.ID[2] = 22;
-         #endif
-         #ifdef TARGET2
-                Arduino1.ID[1] = 0;
-                Arduino1.ID[2] = 24;
-         #endif
-                i++;
-                for (int x = 0; x < sizeof(Arduino1.ID)/sizeof(Arduino1.ID[0]); x++) {
-                        Wire.write(Arduino1.ID[x]);
-                }
-                Serial.print(Arduino1.ID[2]);
-           }else*/if (Arduino1.ID[0] != 0) {
+        if (Arduino1.ID[0] != 0) {
 
                 Arduino1.ID[1]= Arduino1.getFailurecode();
                 Arduino1.ID[2] = Arduino1.problemStatus.Normal;
                 Arduino1.ID[3] = realValue;
-                /*  if(Arduino1.getFailurecode() > 4) {
-                          Arduino1.ID[0] = 0;
-                          Arduino1.ID[1] = 0;
-                          Arduino1.ID[2] = 0;
-                          Arduino1.ID[3] = 0;
-                   }*/
-        }else{
-
+                if(Arduino1.getFailurecode() > 4) {
+                        Arduino1.ID[0] = 0;
+                        Arduino1.ID[1] = 0;
+                        Arduino1.ID[2] = 0;
+                        Arduino1.ID[3] = 0;
+                }
+                Serial.print(Arduino1.ID[0]);
+                Serial.print(Arduino1.ID[1]);
                 Serial.print(Arduino1.ID[2]);
+                Serial.println(Arduino1.ID[3]);
+                /*for (int x = 0; x < sizeof(Arduino1.ID)/sizeof(Arduino1.ID[0]); x++) {
+                     Wire.write(Arduino1.ID[x]);
+                   }*/
+                for (int x = 0; x < sizeof(Arduino1.ID)/sizeof(Arduino1.ID[0]); x++) {
+                        Wire.write(Arduino1.ID[x]);
+                }
+        }else if (Arduino1.ID[0] == 0) {
 
+                Serial.print(Arduino1.ID[0]);
+                Serial.print(Arduino1.ID[1]);
+                Serial.print(Arduino1.ID[2]);
+                Serial.println(Arduino1.ID[3]);
+                for (int x = 0; x < sizeof(Arduino1.ID)/sizeof(Arduino1.ID[0]); x++) {
+                        Wire.write(Arduino1.ID[x]);
+                }
                 #ifdef TARGET1
                 Arduino1.ID[0] = 1;
                 #endif
@@ -98,8 +93,6 @@ void requestEvent()
                 Arduino1.ID[0] = 2;
                 #endif
         }
-        for (int x = 0; x < sizeof(Arduino1.ID)/sizeof(Arduino1.ID[0]); x++) {
-                Wire.write(Arduino1.ID[x]);
-        }
+
 
 }
